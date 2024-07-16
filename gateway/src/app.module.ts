ef4c0b3f1323cdc8641f8074210569534fcd5cb4
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-07-16 13:40:27 +0200

    Refactor database integration

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -4,28 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InternModule } from './intern/intern.module';
 import { PostModule } from './post/post.module';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { PostEntity } from './post/models/post-entity';
 import { PoeModule } from './poe/poe.module';
-import { Poe } from './poe/entities/poe.entity';
+import { DatabaseModule } from './database/database.module';
+import { ConfigurationModule } from './config/configuration.module';
 
 @Module({
-  imports: [InternModule, PostModule, TypeOrmModule.forRoot(AppModule.mariaDBConfig), PoeModule],
+  imports: [InternModule, PostModule, PoeModule, DatabaseModule, ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  public static mariaDBConfig: TypeOrmModuleOptions = {
-    type: 'mariadb',
-    host: 'localhost',
-    port: 3306,
-    username: 'post_db_admin',
-    password: 'admin_db_post',
-    database: 'post_repository',
-    synchronize: true,
-    entities: [
-      PostEntity,
-      Poe
-    ],
-  };
+
 }
